88d4989fac927bf20a902f075bc3078330fe05fa
857 <971165458@qq.com>
2026-04-17 23:24:39 +0800

    Refactor WriteSidebar to include CoverSection

--- src/components/write/components/sidebar.tsx
+++ src/components/write/components/sidebar.tsx
@@ -1,17 +1,20 @@
-import { ConfigSection } from './sections/config-section'
+import { CoverSection } from './sections/cover-section'
 import { MetaSection } from './sections/meta-section'
 import { ImagesSection } from './sections/images-section'
+import { ANIMATION_DELAY, INIT_DELAY } from '@/consts'
 
 type WriteSidebarProps = {
-    categories?: string[]
+	categories?: string[]
 }
 
 export function WriteSidebar({ categories = [] }: WriteSidebarProps) {
-    return (
-        <div className="space-y-6">
-            <ConfigSection />
-            <MetaSection categories={categories} />
-            <ImagesSection />
-        </div>
-    )
+	return (
+		<div className='w-full max-w-[320px] space-y-6'>
+			<div className='grid grid-cols-1 gap-6'>
+				<CoverSection delay={INIT_DELAY + ANIMATION_DELAY * 0} />
+				<MetaSection delay={INIT_DELAY + ANIMATION_DELAY * 1} categories={categories} />
+			</div>
+			<ImagesSection delay={INIT_DELAY + ANIMATION_DELAY * 2} />
+		</div>
+	)
 }
